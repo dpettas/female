b33d525d31ddbacfecea110452c9c7a29747c8ad
Dionisis Pettas <dions.pettas@gmail.com>
2023-08-06 16:44:13 +0300

    Create Lagrange Interpolation  functions

---
+++ src/InterpolationFunctionsLagrange.hpp
@@ -0,0 +1,65 @@
+#ifndef FEMALE_INTERPOLATIONFUNCTIONSLAGRANGE_HPP
+#define FEMALE_INTERPOLATIONFUNCTIONSLAGRANGE_HPP
+
+#include <array>
+#include <functional>
+
+
+// The interpolation functions are based in range of [-1,1]
+namespace female::math::interp{
+    template<int order > 
+    class Lagrange{
+        public:
+            using func_t  = std::function<double(double)>;
+            using funcs_t = std::array<func_t,order>;
+
+            Lagrange( const funcs_t& funcs, const funcs_t& dfuncs);
+
+            std::size_t size() const;
+
+            const func_t&  func(int i) const;
+            const func_t& dfunc(int i) const;
+            
+
+        private:
+            funcs_t m_f;
+            funcs_t m_df;
+    };
+    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+
+    template<int order>
+    Lagrange<order>::Lagrange(const Lagrange::funcs_t& funcs, const Lagrange::funcs_t& dfuncs):
+    m_f(funcs), m_df(dfuncs)
+    {}
+    //-----------------------------------------------------
+    //-----------------------------------------------------
+
+    template<int order>
+    std::size_t Lagrange<order>::size() const {
+        return order;
+    }
+    //-----------------------------------------------------
+    //-----------------------------------------------------
+
+    template<int order>
+    const typename Lagrange<order>::func_t& Lagrange<order>::func(int i) const {
+        return m_f[i];
+    }
+    //-----------------------------------------------------
+    //-----------------------------------------------------
+
+    template<int order>
+    const typename Lagrange<order>::func_t& Lagrange<order>::dfunc(int i) const {
+        return m_df[i];
+    }
+    //-----------------------------------------------------
+    //-----------------------------------------------------
+
+
+
+}
+
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,23 @@
 #include "Vec3.hpp"
 #include "MathematicalOperations.hpp"
 #include "Mesh1D.hpp"
+#include "InterpolationFunctionsLagrange.hpp"
 
 
 int main(int, char**){
     using namespace female;
 
-    female::Mesh1D mesh(Point3(0.0,0.0,0.0), Point3(1.0, 2.00, 3.0), 10);
+    // female::Mesh1D mesh(Point3(0.0,0.0,0.0), Point3(1.0, 2.00, 3.0), 10);
+    female::math::interp::Lagrange<2> phi(
+        { [](double x){  return 0.5 * (1.0 - x); }, 
+          [](double x){  return 0.5 * (1.0 + x); } }, 
 
-    std::cout << mesh << std::endl;
+        { [](double xi){  return -0.50; }, 
+          [](double xi){  return +0.50; } }
+        );
+
+
+    std::cout << phi.func(1)(1.0) << std::endl;
 
     return 0;
 }
